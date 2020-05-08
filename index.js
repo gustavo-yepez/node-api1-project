98c6e7165996c7ff78de4ee0b6d37629f71d6c46
Gustavo Yepez <gustavo@Gustavos-MacBook-Pro.local>
2020-05-07 21:51:07 -0400

    Gustavo

--- index.js
+++ index.js
@@ -34,11 +34,61 @@ server.get('/api/hubs', (requ, res) => {
 })
 
 //-------
-//Delet
+//Delete
 //----------
 server.delete ('/api/hubs/id', (req, res) =>{
     const {id} = req.params;
+
+    const found = req.fin(hub => hub.id === id);
+
+    if (found){
+        hubs = hubs.filter(hub => hub.id === id);
+        res.status(200).json({message: 'deleted'});
+    } else {
+        res.status(404).json({message:"hub not found"});
+    }
+
     console.log(id);
+});
+
+//--------
+//update - patch
+//--------
+
+
+server.patch ('/api/hubs/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    let found = hubs.find(hub => hub.id === id);
+
+    if (found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    }
+    else{
+        res.status(404).json({message:"hub not found"});
+    }
+})
+
+//-------------
+// update - put 
+//------------
+
+server.put ('/api/hubs/id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    let found = hubs.findIndex(hub => hub.id === id);
+
+    if (index !== -1) {
+        changes.id = id;
+        hubs[index] = changes;
+        res.status(200).json(hubs[index]);
+    }
+    else{
+        res.status(404).json({message:"hub not found"});
+    }
 })
 
 
@@ -50,3 +100,8 @@ const PORT = 5000;
 server.listen(PORT, () => {
     console.log('listening on http://localhost:${PORT}')
 });
+
+function newFunction(found, changes, res) {
+    Object.assign(found, changes);
+    res.status(200).json(found);
+}
